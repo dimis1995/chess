462efd26fabad262475e65dd5e65303711c80f83
Dimitris Delis <d.delis@afse.eu>
2022-02-01 16:37:07 +0200

    queen and king movement done

--- main.py
+++ main.py
@@ -115,10 +115,26 @@ def get_all_blocks_between(block1: Block, block2: Block, grid: []):
                 if block.x == block1.x and (block1.y < block.y < block2.y or block2.y < block.y < block1.y):
                     blocks_between.append(block)
     elif block1.x != block2.x and block1.y == block2.y:
+        # horizontal movement
         for column in grid:
             for block in column:
                 if block.y == block1.y and (block1.x < block.x < block2.x or block2.x < block.x < block1.x):
                     blocks_between.append(block)
+    elif block1.x != block2.x and block1.y != block2.y:
+        # diagonal movement
+        if abs(block1.x - block2.x) != abs(block1.y - block2.y):
+            print("SOMETHING HAPPENED, SOMEONE ALERT THE QUEEN")
+            exit(1)
+        x_polarity = block2.x - block1.x
+        y_polarity = block2.y - block1.y
+        for column in grid:
+            for block in column:
+                block_x_polarity = block.x - block1.x
+                block_y_polarity = block.y - block1.y
+                if abs(block_x_polarity) == abs(block_y_polarity):
+                    if (x_polarity * block_x_polarity > 0) and (y_polarity * block_y_polarity > 0):
+                        if abs(block_x_polarity) < abs(x_polarity) and abs(block_y_polarity) < abs(y_polarity):
+                            blocks_between.append(block)
     return blocks_between
 
 
@@ -146,6 +162,97 @@ def check_rook_rules(piece, pieces, grid, block_to_be_populated):
         return False
 
 
+def check_bishop_rules(piece, pieces, grid, block_to_be_populated):
+    if (piece.board_block.x == block_to_be_populated.x) or (piece.board_block.y == block_to_be_populated.y):
+        print("rook can't move horizontally or vertically")
+        return False
+    if abs(piece.board_block.x - block_to_be_populated.x) == abs(piece.board_block.y - block_to_be_populated.y):
+        blocks_in_between = get_all_blocks_between(piece.board_block, block_to_be_populated, grid)
+        for block in blocks_in_between:
+            if block.has_chess_piece:
+                print("colliding with block: " + str(block))
+                return False
+        if block_to_be_populated.has_chess_piece:
+            dead_piece_walking = get_piece(pieces, (block_to_be_populated.x + 30, block_to_be_populated.y + 30))
+            if dead_piece_walking.white == piece.white:
+                print("pieces of same color can't kill each other")
+                return False
+            else:
+                remove_piece(pieces, dead_piece_walking)
+                print("piece killed: " + str(dead_piece_walking))
+                return True
+        else:
+            return True
+
+
+def check_queen_rules(piece, pieces, grid, block_to_be_populated):
+    if (piece.board_block.x == block_to_be_populated.x) or (piece.board_block.y == block_to_be_populated.y):
+        blocks_in_between = get_all_blocks_between(piece.board_block, block_to_be_populated, grid)
+        for block in blocks_in_between:
+            if block.has_chess_piece:
+                print("colliding with block: " + str(block))
+                return False
+        if block_to_be_populated.has_chess_piece:
+            dead_piece_walking = get_piece(pieces, (block_to_be_populated.x + 30, block_to_be_populated.y + 30))
+            if dead_piece_walking.white == piece.white:
+                print("pieces of same color can't kill each other")
+                return False
+            else:
+                remove_piece(pieces, dead_piece_walking)
+                print("piece killed: " + str(dead_piece_walking))
+                return True
+        else:
+            return True
+    if abs(piece.board_block.x - block_to_be_populated.x) == abs(piece.board_block.y - block_to_be_populated.y):
+        blocks_in_between = get_all_blocks_between(piece.board_block, block_to_be_populated, grid)
+        for block in blocks_in_between:
+            if block.has_chess_piece:
+                print("colliding with block: " + str(block))
+                return False
+        if block_to_be_populated.has_chess_piece:
+            dead_piece_walking = get_piece(pieces, (block_to_be_populated.x + 30, block_to_be_populated.y + 30))
+            if dead_piece_walking.white == piece.white:
+                print("pieces of same color can't kill each other")
+                return False
+            else:
+                remove_piece(pieces, dead_piece_walking)
+                print("piece killed: " + str(dead_piece_walking))
+                return True
+        else:
+            return True
+
+
+def check_king_rules(piece, pieces, grid, block_to_be_populated):
+    if abs(piece.board_block.x - block_to_be_populated.x) > 150 or \
+            abs(piece.board_block.y - block_to_be_populated.y) > 150:
+        print("king can't move more than one block space")
+        return False
+    if (piece.board_block.x == block_to_be_populated.x) or (piece.board_block.y == block_to_be_populated.y):
+        if block_to_be_populated.has_chess_piece:
+            dead_piece_walking = get_piece(pieces, (block_to_be_populated.x + 30, block_to_be_populated.y + 30))
+            if dead_piece_walking.white == piece.white:
+                print("pieces of same color can't kill each other")
+                return False
+            else:
+                remove_piece(pieces, dead_piece_walking)
+                print("piece killed: " + str(dead_piece_walking))
+                return True
+        else:
+            return True
+    if abs(piece.board_block.x - block_to_be_populated.x) == abs(piece.board_block.y - block_to_be_populated.y):
+        if block_to_be_populated.has_chess_piece:
+            dead_piece_walking = get_piece(pieces, (block_to_be_populated.x + 30, block_to_be_populated.y + 30))
+            if dead_piece_walking.white == piece.white:
+                print("pieces of same color can't kill each other")
+                return False
+            else:
+                remove_piece(pieces, dead_piece_walking)
+                print("piece killed: " + str(dead_piece_walking))
+                return True
+        else:
+            return True
+
+
 def move(piece: Piece, pieces: [], grid: [], new_pos: (int, int)):
     block_to_be_populated: Block
     block_to_be_populated = get_block(grid, new_pos)
@@ -158,6 +265,12 @@ def move(piece: Piece, pieces: [], grid: [], new_pos: (int, int)):
     elif type(piece) == Knight and check_knight_rules(piece, pieces, grid, block_to_be_populated):
         check = True
     elif type(piece) == Rook and check_rook_rules(piece, pieces, grid, block_to_be_populated):
+        check = True
+    elif type(piece) == Bishop and check_bishop_rules(piece, pieces, grid, block_to_be_populated):
+        check = True
+    elif type(piece) == Queen and check_queen_rules(piece, pieces, grid, block_to_be_populated):
+        check = True
+    elif type(piece) == King and check_king_rules(piece, pieces, grid, block_to_be_populated):
         check = True
     else:
         print("error, see error log")
